ba931fd6fcd84707060683aa44ea73fb3cb36043
akzhol0 <122439105+akzhol0@users.noreply.github.com>
2023-01-12 08:53:49 -0800

    Update index_core.js

--- java_script/index_core.js
+++ java_script/index_core.js
@@ -3,33 +3,33 @@ let first_num, second_num;
 let result, move;
 
 function one() {
-    right_num+= "1"
+    right_num += "1"
 
     document.getElementById('res').innerHTML = right_num;
 }
 
 function two() {
-    right_num+= "2"
+    right_num += "2"
     document.getElementById('res').innerHTML = right_num;
 }
 
 function three() {
-    right_num+= "3"
+    right_num += "3"
     document.getElementById('res').innerHTML = right_num;
 }
 
 function four() {
-    right_num+= "4"
+    right_num += "4"
     document.getElementById('res').innerHTML = right_num;
 }
 
 function five() {
-    right_num+= "5"
+    right_num += "5"
     document.getElementById('res').innerHTML = right_num;
 }
 
 function six() {
-    right_num+= "6"
+    right_num += "6"
     document.getElementById('res').innerHTML = right_num;
 }
 
@@ -39,27 +39,27 @@ function seven() {
 }
 
 function eight() {
-    right_num+= "8"
+    right_num += "8"
     document.getElementById('res').innerHTML = right_num;
 }
 
 function nine() {
-    right_num+= "9"
+    right_num += "9"
     document.getElementById('res').innerHTML = right_num;
 }
 
 function dot() {
-    right_num+= "."
+    right_num += "."
     document.getElementById('res').innerHTML = right_num;
 }
 
 function percent() {
-    right_num/= 100
+    right_num /= 100
     document.getElementById('res').innerHTML = right_num;
 }
 
 function dicr() {
-    right_num+= "-"
+    right_num += "-"
     document.getElementById('res').innerHTML = right_num;
 }
 
@@ -69,7 +69,7 @@ function ac() {
 }
 
 function zero() {
-    right_num+= "0"
+    right_num += "0"
     document.getElementById('res').innerHTML = right_num;
 }
 
